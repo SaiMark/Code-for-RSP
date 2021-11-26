3d6f55de82469671f8d2271a43478d5c58ebab38
SaiMark <59070894+SaiMark@users.noreply.github.com>
2021-11-26 22:15:52 +1030

    Demo graph
    implementation for demo graph

--- Data-Structures-/DFS/main.cpp
+++ Data-Structures-/DFS/main.cpp
@@ -2,6 +2,17 @@
 #include<vector>
 using namespace std;
 
+/*
+    Demo graph
+
+    0-----3
+    |\   /|
+    | \ / |
+    |  4  |
+    | / \ |
+    |/   \|
+    1-----2
+*/
 vector<int>adj[5];
 bool visited[5];
 
@@ -56,9 +67,14 @@ void printList(vector<int>adj[], int V){
 int main()
 {
 	addEdge(adj,0,1);
-	addEdge(adj,1,3);
-	addEdge(adj,3,2);
-	addEdge(adj,2,4);
+    addEdge(adj,1,4);
+    addEdge(adj,0,4);
+    addEdge(adj,0,3);
+    addEdge(adj,1,2);
+    addEdge(adj,4,3);
+    addEdge(adj,4,2);
+    addEdge(adj,3,2);
+	
 	//cout<<adj.size()<<endl;
 	for(int i=0;i<5;i++){
 		if(!visited[i]){
