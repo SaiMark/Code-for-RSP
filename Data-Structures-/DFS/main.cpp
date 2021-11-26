600f66e99cec18bac63b265c0ac1751c228115bc
SaiMark <59070894+SaiMark@users.noreply.github.com>
2021-11-26 22:01:00 +1030

    DFS
    recursion of DFS

--- Data-Structures-/DFS/main.cpp
+++ Data-Structures-/DFS/main.cpp
@@ -2,46 +2,70 @@
 #include<vector>
 using namespace std;
 
-void addEdge(vector<vector<int>> adj, int s, int d) {
+vector<int>adj[5];
+bool visited[5];
+
+void addEdge(vector<int>adj[], int s, int d) {
   adj[s].push_back(d);
   adj[d].push_back(s);
 }
 
-void DFS(vector<vector<int>>adj, vector<bool>visited, int src){
+// void dfs_r(int s){
+//     visited[s] = true; //Mark first node as visited
+//     cout << s << " ";
+//     for(int i = 0; i < vec[s].size(); i++){ //Vector of adjency nodes
+//         if(visited[vec[s][i]] == false) dfs_r(vec[s][i]); //If not visited, recurse it
+//     }
+// }
+/*
+//Iteration
+void dfs(const vector<vector<int>> &vec, int s){
+    stack<int> stk;
+    stk.push(s); //Push start node
+    visited[s] = true; //Mark start node
+    while(!stk.empty()){ //When stack not empty
+        int node = stk.top(); //Get top
+        stk.pop(); //Pop top
+        cout << node << " ";
+        for(int i = 0; i < vec[node].size(); ++i){ //Vector of adjency nodes
+            if(!visited[vec[node][i]]){ //If adjency node is not visited
+                stk.push(vec[node][i]); //Push this node into stack
+                visited[vec[node][i]] = true; //Mark as visited
+            }
+        }
+    }
+}*/
+ void initialize() {
+    for(int i = 0;i < 10;++i){
+        visited[i] = false;
+    }
+}
+
+void DFS(int src){
 	visited[src] = true;
-	cout<<src<<"->";
-	for(int i=0;i<adj[src].size();i++){
-		int temp = adj[src][i];
-		if(!visited[temp]){
-			DFS(adj,visited,temp);
+	for(int i=0 ;i<adj[src].size(); i++){
+		if(!visited[adj[src][i]]){
+            cout<<adj[src][i]<<"->";
+			DFS(adj[src][i]);
 		}
 	}
 }
 
 int main()
 {
-	int V = 5;
-	vector<vector<int>>adj(0,vector<int>(V));
-	vector<bool>visited(false,V);
-
 	addEdge(adj,0,1);
-	addEdge(adj,0,2);
 	addEdge(adj,0,3);
+	addEdge(adj,0,2);
 	addEdge(adj,1,2);
 	addEdge(adj,3,2);
 	addEdge(adj,2,4);
 	addEdge(adj,3,4);
-
-	for(int i=0;i<adj.size();i++){
-		for(int j=0;j<adj[i].size();j++){
-			cout<<adj[i][j]<<" ";
+	//cout<<adj.size()<<endl;
+	for(int i=0;i<5;i++){
+		if(!visited[i]){
+			DFS(i);
 		}
-		cout<<endl;
 	}
-	for(int i=0;i<visited.size();i++){
-		cout<<visited[i]<<" ";
-	}
-	//DFS(adj,visited,0);
 
 
 	return 0;
